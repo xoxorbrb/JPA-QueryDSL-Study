94cbb8eb7d801885da68766e29d27dc641c023cd
xoxorbrb <tg3782@naver.com>
2024-12-22 23:13:03 +0900

    JPA 기본 CRUD 테스트

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -1,18 +1,68 @@
 package hellojpa;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+import javax.persistence.*;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello"); // 설정파일에 설정했던 unit 이름
+        // 애플리케이션로딩 시점에 하나만
 
-        EntityManager em = emf.createEntityManager();
-        //code 작성
+        EntityManager em = emf.createEntityManager(); //트랜잭션 마다 만들어줘야함 (DB커넥션을 얻어서 쿼리를 날리고 종료하는)
 
-        em.close();
+        EntityTransaction tx = em.getTransaction(); // 트랜잭션 생성 * 항상 JPA는 트랜잭션 안에서 동작해야함
+        tx.begin();
 
+        try {
+
+            /*
+            전체 회원 조회
+             */
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .getResultList();
+
+            for (Member member : result) {
+                System.out.println("member.name = " + member.getName());
+            }
+            /*insert
+//            Member member = new Member();
+//            member.setId(2L);
+//            member.setName("HelloB");
+//
+//            em.persist(member);
+//            tx.commit();
+
+            //update
+            Member findMember = em.find(Member.class, 1L);
+            findMember.setName("HelloJPA");
+
+            //select
+            System.out.println("findMember.id = " + findMember.getId());
+            System.out.println("findMember.name = " + findMember.getName());
+
+
+            //delete
+//            em.remove(findMember);
+
+            tx.commit();
+             */
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close(); // 꼭 사용해야함
+        }
         emf.close();
+
+        /*  설정파일의 show_sql - sql 보여주기, format_sql - 이쁘게 정리,
+        Hibernate:
+         //insert hellojpa.Member  // use_sql_comments - 왜 나온지 확인
+         insert
+                into
+        Member
+                (name, id)
+        values
+                (?, ?)
+         */
+
     }
 }
---
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -0,0 +1,33 @@
+package hellojpa;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity // 꼭 필요한 어노테이션
+//@Table(name="USER") 테이블 이름이 클래스와 다를때 설정
+public class Member {
+
+    @Id //Primary key가 뭔지 JPA에 알려주기 위한 어노테이션
+    private Long id;
+
+    //@Column(name = "username") 컬럼이 다를때사용
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}

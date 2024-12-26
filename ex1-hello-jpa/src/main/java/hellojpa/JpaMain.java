c942d084a1d255faf4193c6d8296fbf027b0efeb
xoxorbrb <tg3782@naver.com>
2024-12-26 23:09:25 +0900

    영속 비영속 확인

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -15,15 +15,15 @@ public class JpaMain {
 
         try {
 
-            /*
-            전체 회원 조회
+            /***
+             * 전체 회원 조회
              */
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .getResultList();
-
-            for (Member member : result) {
-                System.out.println("member.name = " + member.getName());
-            }
+//            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+//                    .getResultList();
+//
+//            for (Member member : result) {
+//                System.out.println("member.name = " + member.getName());
+//            }
             /*insert
 //            Member member = new Member();
 //            member.setId(2L);
@@ -46,6 +46,28 @@ public class JpaMain {
 
             tx.commit();
              */
+
+            /***
+             *  엔티티 생명주기
+             */
+
+            //비영속
+            Member member = new Member();
+            member.setId(101L);
+            member.setName("HelloJPA");
+
+            //영속
+            System.out.println("=== BEFORE ===");
+            em.persist(member);
+//            em.detach(member); // 영속성 컨텍스트에서 분리, 준영속 상태
+            System.out.println("=== AFTER ===");
+
+            Member findMember = em.find(Member.class, 101L);
+            System.out.println("findMember id => " + findMember.getId());
+            System.out.println("findMember name => " + findMember.getName());
+
+
+            tx.commit();
         } catch (Exception e) {
             tx.rollback();
         } finally {
